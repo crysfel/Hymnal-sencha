3ea5042a8f094fba633c3eb44c879bbf5ad5fd68
Crysfel Villa <crysfel@bleext.com>
2014-02-20 16:25:13 -0500

    added the debug directive to the config file

--- app/Config.js
+++ app/Config.js
@@ -5,8 +5,10 @@
 Ext.define('Hymnal.Config', {
     singleton   : true,
 
-    //LYRICS_URL   : 'http://demos.bleext.com/hymnal/index.php/hymnal/findAll',
-    LYRICS_URL   : 'http://10.0.0.6:3000/api/hymns/search',
+    LYRICS_URL   : 'http://demos.bleext.com/hymnal/index.php/hymnal/findAll',
+    //<debug>
+    LYRICS_URL   : 'http://localhost:3000/api/hymns/search',
+    //</debug>
     VERSION_URL  : 'http://demos.bleext.com/hymnal/index.php/hymnal/latestVersion',
     TIMEOUT      : 1000, //Time to wait for the next page
     PAGE_SIZE    : 50,

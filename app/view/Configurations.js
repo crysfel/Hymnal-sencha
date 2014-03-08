68873c50c1bc3afe01be798f87069f7ea5857590
Crysfel Villa <crysfel@bleext.com>
2014-03-07 21:45:12 -0500

    Styling the configuration screen

--- app/view/Configurations.js
+++ app/view/Configurations.js
@@ -21,41 +21,49 @@ Ext.define('Hymnal.view.Configurations',{
             {title:'Configuraciones'}
         ],
 		items	: [{
-			xtype	: 'fieldset',
-			defaults: {labelWidth:'50%'},
-			items	: [{
-				xtype	: 'spinnerfield',
-				label	: 'Tamaño fuente',
-				minValue: 20,
-				maxValue: 100,
-				value	: 40,
-				stepValue: 10,
-				name	: 'fontSize'
-			},{
-				xtype: 'selectfield',
-				label: 'Fondo',
-				name : 'background',
-				options: [
-					{text: 'Blanco',  value: 'bg-white'},
-					{text: 'Negro', value: 'bg-black'},
-					{text: 'Sepia',  value: 'bg-sepia'}
-				]
-			},{
-                xtype: 'radiofield',
-                name : 'music',
-                value: 'music',
-                label: 'Reproducir pista'
+            xtype : 'container',
+            layout: 'auto',
+            items : [{
+                xtype   : 'fieldset',
+                defaults: {labelWidth:'50%'},
+                cls     : 'custom-panel',
+                items   : [{
+                    xtype   : 'spinnerfield',
+                    label   : 'Tamaño fuente',
+                    minValue: 20,
+                    maxValue: 100,
+                    value   : 40,
+                    stepValue: 10,
+                    name    : 'fontSize'
+                },{
+                    xtype: 'selectfield',
+                    label: 'Fondo',
+                    name : 'background',
+                    options: [
+                        {text: 'Blanco',  value: 'bg-white'},
+                        {text: 'Negro', value: 'bg-black'},
+                        {text: 'Sepia',  value: 'bg-sepia'}
+                    ]
+                },{
+                    xtype: 'radiofield',
+                    name : 'music',
+                    value: 'voice',
+                    label: 'Himno Cantado'
+                },{
+                    xtype: 'radiofield',
+                    name : 'music',
+                    value: 'music',
+                    label: 'Tocar Pista'
+                }]
             },{
-                xtype: 'radiofield',
-                name : 'music',
-                value: 'voice',
-                label: 'Reproducir pista cantada'
-            }]
-		},{
-			xtype : 'container',
-			cls   : 'about',
-			docked	: 'bottom',
-			html  : 'Esta aplicación ha sido desarrollada por Bleex S de RL MI'
-		}]
+                xtype: 'component',
+                cls  : 'custom-panel',
+                html : [
+                    '<h4>Sobre esta app</h4>',
+                    '<p>Esta app ha sido desarrollada para el beneficio de todos aquellos que quieran tener una devoción personal con Dios por medio del canto.</p>',
+                    '<p>De parte de los desarrolladores les enviamos un fraternal saludo esperando que esta app sea de gran bendición.</p>'
+                ].join('')
+            }]     
+        }]
     }
 });
